053a81023573f594dd700488cfdcae5dc23e8206
Joseph Amason <joseph.amason@gmail.com>
2015-12-25 02:09:12 +0000

    Working app

--- src/ServerRequest/Scripts/app.js
+++ src/ServerRequest/Scripts/app.js
@@ -1,14 +1,17 @@
-﻿(function () {
-    'use strict';
+﻿var app = angular.module("app", []);
+app.controller('requestController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
 
-    angular.module('serverRequestApp', [
-        // Angular modules 
-        //'ngRoute'
+    $scope.itemCollection = [];
+    console.log(JSON.stringify('Start jsonp to /api/request'));
+    $http({
+        url: 'api/request',
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    }).then(function (responce) {
+        $scope.itemCollection = responce.data;
+        console.log("Values inside of $http scope: " + $scope.itemCollection.length);
+    }).finally(function () {
+        console.log("Values outside of $http scope: " + $scope.itemCollection.length);
+    });
 
-        // Custom modules 
-        "requestService"
-
-        // 3rd Party Modules
-        
-    ]);
-})();
+}]);
