38db5cc0f13d32b46acf3671dfc8b59416678fa5
Joseph Amason <joseph.amason@gmail.com>
2015-12-25 08:13:29 +0000

    Update Static Request Data & Request Model

--- src/ServerRequest/Models/Request.cs
+++ src/ServerRequest/Models/Request.cs
@@ -7,11 +7,22 @@ namespace ServerRequest.Models
 {
     public class Request
     {
-        internal string requestor;
-
         public int Id { get; set; }
         public string ServerName { get; set; }
         public string Requestor { get; set; }
+        public string Domain { get; set; }
+        public string Datacenter { get; set; }
+        public string OS { get; set; }
+        public int Cores { get; set; }
+        public int RAM { get; set; }
+        public List<DriveConfig> Drives { get; set; }
+    }
 
+    public class DriveConfig
+    {
+        public Int64 Id { get; set; }
+        public int Size { get; set; }
+        public string DriveId { get; set; }
+        public string DisplayName { get; set; }
     }
 }
